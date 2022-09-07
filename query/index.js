fd401e7f59e4f61553941f07efda5a452c85cd76
brkgcl <burakguclu05@hotmail.com>
2022-09-07 16:37:16 +0300

    finish

--- query/index.js
+++ query/index.js
@@ -1,51 +1,64 @@
-const express = require('express');
-const BodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-app.use(BodyParser.json());
+app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
 
-// quicj examples
-// posts === {
-//     'j123j42': {
-//         id: 'j123j42',
-//         title: 'post title',
-//         comment: [
-//             { id: 'j123j42', content: 'comment !' }
-//         ]
-//     }
-// }
+    posts[id] = { id, title, comments: [] };
+  }
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
 
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
 
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
 
-    if (type === 'PostCreated') {
-        const { id, title } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
 
-        posts[id] = { id, title, comments: [] };
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
+  res.send({});
+});
+
+app.listen(4002, async () => {
+  console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
     }
-
-    if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
-
-        const post = posts[postId];
-        post.comments.push({ id, content });
-    }
-
-    console.log(posts);
-
-    res.send({ })
-})
-
-app.listen(4002, () => {
-    console.log("Listening on 4002");
-})
+  } catch (error) {
+    console.log(error.message);
+  }
+});
